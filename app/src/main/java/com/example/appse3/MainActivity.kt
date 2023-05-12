b48302c0161ed13e31ff9f725d87eb808783a713
johannasimml <johanna@simml.net>
2023-05-12 10:11:39 +0200

    alles geht wieder

--- app/src/main/java/com/example/appse3/MainActivity.kt
+++ app/src/main/java/com/example/appse3/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.appse3
 
 import android.os.Bundle
+import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
