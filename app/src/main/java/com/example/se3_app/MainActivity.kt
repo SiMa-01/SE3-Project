dbaf91a927393ecfadeb8d101b8944a5f425c303
SiMa-01 <90407617+SiMa-01@users.noreply.github.com>
2023-05-30 10:13:47 +0200

    firebase merge

--- app/src/main/java/com/example/se3_app/MainActivity.kt
+++ app/src/main/java/com/example/se3_app/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.example.se3_app
 
-import android.content.ContentValues.TAG
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,26 +8,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.se3_app.View.StartView.StartView
-import com.example.se3_app.View.StartView.StartViewModel
-
-
 import com.example.se3_app.ui.theme.SE3AppTheme
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.se3_app.CocktailSearchView.CocktailSearchView
-import com.example.se3_app.CocktailSearchView.CocktailSearchViewModel
-import com.example.se3_app.EinkaufslitenView.EinkaufslistenView
-import com.example.se3_app.EinkaufslitenView.EinkaufslistenViewModel
-import com.example.se3_app.HinzufuegenView.HinzufuegenView
-import com.example.se3_app.HinzufuegenView.HinzufuegenViewModel
-import com.example.se3_app.MerklistenView.MerklistenView
-import com.example.se3_app.MerklistenView.MerklistenViewModel
-import com.example.se3_app.ResultView.ResultView
-import com.example.se3_app.ResultView.ResultViewModel
-import com.example.se3_app.RezeptView.RezeptView
-import com.example.se3_app.RezeptView.RezeptViewModel
+import com.example.se3_app.cocktailSearchView.CocktailSearchView
+import com.example.se3_app.cocktailSearchView.CocktailSearchViewModel
+import com.example.se3_app.einkaufslitenView.EinkaufslistenView
+import com.example.se3_app.einkaufslitenView.EinkaufslistenViewModel
 import com.example.se3_app.firebase.EmailPasswordActivity
 import com.example.se3_app.firebase.forgot_password.ForgotPasswordView
 import com.example.se3_app.firebase.forgot_password.ForgotPasswordViewModel
@@ -41,25 +25,16 @@ import com.example.se3_app.firebase.sign_up.SignUpView
 import com.example.se3_app.firebase.sign_up.SignUpViewModel
 import com.example.se3_app.firebase.verify_email.VerifyEmailView
 import com.example.se3_app.firebase.verify_email.VerifyEmailViewModel
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.FirebaseException
-import com.google.firebase.auth.ActionCodeSettings
-import com.google.firebase.auth.AuthCredential
-import com.google.firebase.auth.EmailAuthProvider
-import com.google.firebase.auth.FacebookAuthProvider
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GithubAuthProvider
-import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.PhoneAuthCredential
-import com.google.firebase.auth.PhoneAuthOptions
-import com.google.firebase.auth.PhoneAuthProvider
-import com.google.firebase.auth.PlayGamesAuthProvider
-import com.google.firebase.auth.ktx.actionCodeSettings
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.auth.ktx.userProfileChangeRequest
-import com.google.firebase.ktx.Firebase
-import java.util.concurrent.TimeUnit
+import com.example.se3_app.hinzufuegenView.HinzufuegenView
+import com.example.se3_app.hinzufuegenView.HinzufuegenViewModel
+import com.example.se3_app.merklistenView.MerklistenView
+import com.example.se3_app.merklistenView.MerklistenViewModel
+import com.example.se3_app.resultView.ResultView
+import com.example.se3_app.resultView.ResultViewModel
+import com.example.se3_app.rezeptView.RezeptView
+import com.example.se3_app.rezeptView.RezeptViewModel
+import com.example.se3_app.startView.StartView
+import com.example.se3_app.startView.StartViewModel
 
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/se3_app/cto/CocktailDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.se3_app.cto;
-
-public class CocktailDto {
-}
