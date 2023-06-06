11d8ad7d13b461428e9e9cd4d0576709dbebdc7a
SiMa-01 <90407617+SiMa-01@users.noreply.github.com>
2023-06-06 21:03:39 +0200

    update SpashScreen logo

--- app/src/main/java/com/example/se3_app/loadingScreen/SplashScreen.kt
+++ app/src/main/java/com/example/se3_app/loadingScreen/SplashScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -25,8 +26,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.se3_app.R
 import com.example.se3_app.startView.StartViewModel
 import kotlinx.coroutines.delay
@@ -34,7 +39,6 @@ import kotlinx.coroutines.delay
 @Composable
 fun AnimatedSplashScreen(navController: NavController, startViewModel: StartViewModel) {
     var startAnimation by remember { mutableStateOf(false) }
-    val mussHierhin: String
 
     val alphaAnim = animateFloatAsState(
         targetValue = if (startAnimation) 1f else 0f,
@@ -46,10 +50,11 @@ fun AnimatedSplashScreen(navController: NavController, startViewModel: StartView
     LaunchedEffect(key1 = true) {
         startAnimation = true
         startViewModel.getAllCocktails()
+        delay(3000)
 
 
         while (startViewModel.loading) {
-            delay(500) // Wartezeit vor der erneuten Überprüfung
+            delay(1000) // Wartezeit vor der erneuten Überprüfung
         }
 
         navController.popBackStack()
@@ -62,18 +67,18 @@ fun AnimatedSplashScreen(navController: NavController, startViewModel: StartView
 fun Splash(alpha: Float, startViewModel: StartViewModel) {
     Box(
         modifier = Modifier
-            .background(Color(0xFFAAD6E3))
+            .background(Color.White)
             .fillMaxSize(),
-        contentAlignment = Alignment.Center
     ) {
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 100.dp)
+                .fillMaxSize()
+                .padding(vertical = 100.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
             Image(
-                painter = painterResource(id = R.drawable.cocktail),
+                painter = painterResource(id = R.drawable.logo_app),
                 contentDescription = "App Logo",
                 modifier = Modifier
                     .fillMaxSize()
@@ -81,15 +86,12 @@ fun Splash(alpha: Float, startViewModel: StartViewModel) {
                     .align(Alignment.CenterHorizontally)
             )
 
-
-
             if (startViewModel.loading) {
                 LinearProgressIndicator(
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(horizontal = 16.dp),
-                    trackColor = Color.Blue,
-                    color = Color.Black
+                    color = Color.Blue //progress color
                 )
             }
         }
