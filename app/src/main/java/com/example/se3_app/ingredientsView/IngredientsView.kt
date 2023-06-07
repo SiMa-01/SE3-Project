356b41a0e2070d4d0d18c68b10846aa1046398ea
SiMa-01 <90407617+SiMa-01@users.noreply.github.com>
2023-06-07 15:23:34 +0200

    Ingredients View update

--- app/src/main/java/com/example/se3_app/ingredientsView/IngredientsView.kt
+++ app/src/main/java/com/example/se3_app/ingredientsView/IngredientsView.kt
@@ -1,10 +1,10 @@
 package com.example.se3_app.ingredientsView
+
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
@@ -15,21 +15,19 @@ import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.se3_app.ui.theme.Purple80
 import com.example.se3_app.ui.theme.PurpleGrey40
-import kotlinx.coroutines.delay
+//import androidx.compose.foundation.layout.FlowRow
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -56,7 +54,7 @@ fun IngredientsView(navController: NavController, ingredientsViewModel: Ingredie
 }
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun ChipEachRow(
     list: List<String>,
@@ -65,14 +63,11 @@ fun ChipEachRow(
 
     var multipleChecked by rememberSaveable { mutableStateOf(tempList) }
 
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(20.dp),
-    ) {
+    Row(modifier = Modifier.padding(8.dp)) {
         list.forEachIndexed { index, s ->
             FilterChip(
-                selected = multipleChecked.contains(index), onClick = {
+                selected = multipleChecked.contains(index),
+                onClick = {
                     multipleChecked = if (multipleChecked.contains(index))
                         multipleChecked.minus(index)
                     else
